2f2afd177e47be0f7f453cbdad41824dc26e8e0e
rufaidah-jomaa <rufaidah.a.jomaa@gmail.com>
2025-02-04 11:55:57 +0200

    js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,18 @@
-const mainProduct = document.querySelector('.main-product')
+const itemsWithAnimation = document.querySelectorAll('.animation')
+window.addEventListener('scroll', function () {
+    itemsWithAnimation.forEach(element => {
+        const elementTop = element.getBoundingClientRect().top;
+        if (elementTop < window.innerHeight - 100) { //  عندما يكون العنصر مرئي جزئيًا
+            element.classList.add('fadeup-animation');
+        }
 
-window.addEventListener('scroll', function() {
-    main
-});
+    });
+
+})
+
+const latestNews=document.querySelector('.container-news');
+window.addEventListener('scroll',function(){
+    if(window.innerHeight > latestNews.getBoundingClientRect().top ){
+      latestNews.classList.add('pop-animation');
+    }
+})
